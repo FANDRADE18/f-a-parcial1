d0c5c7a0ad3023da73af4b920ec194e4f5116f80
Fernando Andrade <fandradetv@gmail.com>
2022-06-20 07:40:46 -0500

    Ecommerce F&A V1

---
+++ app/src/main/java/com/fa/fyaparcial1/ManActivity.java
@@ -0,0 +1,14 @@
+package com.fa.fyaparcial1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man);
+    }
+}
